49ee208109c68153b35b6840c9046fce5b083c67
HwangRock <157935545+HwangRock@users.noreply.github.com>
2025-03-01 02:12:17 +0900

    chore: 의존성, DB 연결, 도커 생성

---
+++ src/main/java/com/example/postserver/PostserverApplication.java
@@ -0,0 +1,13 @@
+package com.example.postserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostserverApplication.class, args);
+	}
+
+}
